74a3ab65ed5a6789da45c6925c23e0f5480b1bc2
Konstantin Solovei <SoloveiKonstantin@gmail.com>
2022-01-31 16:24:49 +0300

    Repeat of numbers check

--- tasks/medium/yes_no.py
+++ tasks/medium/yes_no.py
@@ -2,3 +2,13 @@
 Напишите функцию yes_or_no, которая принимает список из целых чисел,
 проходит по нему и выводит Yes, если число уже встречалось и No, если нет
 """
+
+
+def yes_or_no(some_list: list):
+    for i in range(len(some_list)):
+        result = "No"
+        for n in range(i):
+            if some_list[i] == some_list[n]:
+                result = "Yes"
+                break
+        print(result)
