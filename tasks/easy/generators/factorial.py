08cff1c7e39e5c89d10378ec8d1ff853f3e0f259
Konstantin Solovei <SoloveiKonstantin@gmail.com>
2022-01-31 16:11:09 +0300

    Factorial generator

--- tasks/easy/generators/factorial.py
+++ tasks/easy/generators/factorial.py
@@ -10,3 +10,12 @@ next(factorial_gen) -> 2
 next(factorial_gen) -> 6
 next(factorial_gen) -> 24
 """
+
+
+def factorial():
+    i = 1
+    factor = 1
+    while True:
+        factor *= i
+        i += 1
+        yield factor
